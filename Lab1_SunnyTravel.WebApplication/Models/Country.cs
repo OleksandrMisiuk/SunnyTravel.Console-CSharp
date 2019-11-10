811d0d93fbeacde39a9603005a20a3f81d634c50
Oleksandr Misiuk <santa.misuklord@gmail.com>
2019-11-10 17:23:22 +0200

    Created WebApi Project

---
+++ Lab1_SunnyTravel.WebApplication/Models/Country.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab1_SunnyTravel.Core.Entity
+{
+    public class Country
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public virtual ICollection<City> Cities { get; set; }
+    }
+}
---
+++ Lab1_SunnyTravel.WebApplication/Models/Hotel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Lab1_SunnyTravel.Core.Entity
+{
+    public class Hotel
+    {
+        [Column("id")]
+        public int Id { get; set; }
+
+        [Column("name")]
+        public string Name { get; set; }
+
+        [Column("sea_distance")]
+        public int SeaDistance { get; set; }
+
+        [Column("rating")]
+        public int Rating { get; set; }
+
+        [ForeignKey("city_id")]
+        public City City { get; set; }
+
+        public List<Room> Rooms { get; set; }
+
+        public List<Tour> Tours { get; set; }
+
+        public List<Meal> Meals { get; set; }
+    }
+}
